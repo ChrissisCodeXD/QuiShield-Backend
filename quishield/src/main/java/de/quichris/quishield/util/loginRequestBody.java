7f45250e19cf94af34ca05f7ec2f7c8af969a953
Chrissi <schwarzlichtampel@gmail.com>
2022-07-14 15:55:30 +0200

    loginRequestBody.java: Added a Class that represents the request body of the /login route

---
+++ quishield/src/main/java/de/quichris/quishield/util/loginRequestBody.java
@@ -0,0 +1,28 @@
+package de.quichris.quishield.util;
+
+
+import lombok.Data;
+
+@Data
+public class loginRequestBody {
+
+    private String email;
+
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
