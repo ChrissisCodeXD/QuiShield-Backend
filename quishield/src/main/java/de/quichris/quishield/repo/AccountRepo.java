31ecfe0056b6848f49c5efc1bde89a64a9035dda
Chrissi <schwarzlichtampel@gmail.com>
2022-07-14 15:35:37 +0200

    AccountRepo.java: Added repo for the Accounts

---
+++ quishield/src/main/java/de/quichris/quishield/repo/AccountRepo.java
@@ -0,0 +1,12 @@
+package de.quichris.quishield.repo;
+
+import de.quichris.quishield.entity.Account;
+import de.quichris.quishield.exceptions.AccountNotFound;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AccountRepo extends JpaRepository<Account,String> {
+    Optional<Account> findByEmail(String email) throws AccountNotFound;
+
+}
