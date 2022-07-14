da804a59e4d4c1c34fd5f7a013d2804b521dfd71
ChrissisCodeXD <88249929+ChrissisCodeXD@users.noreply.github.com>
2022-07-14 11:45:29 +0200

    Add files via upload

---
+++ quishield/src/main/java/de/quichris/quishield/QuishieldApplication.java
@@ -0,0 +1,13 @@
+package de.quichris.quishield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuishieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuishieldApplication.class, args);
+	}
+
+}
---
+++ quishield/src/test/java/de/quichris/quishield/QuishieldApplicationTests.java
@@ -0,0 +1,13 @@
+package de.quichris.quishield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuishieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
