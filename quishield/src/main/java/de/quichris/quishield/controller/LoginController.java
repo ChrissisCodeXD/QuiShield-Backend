9f377eb485e70fc7020ef2c9f8d896cc8516a3c4
Chrissi <schwarzlichtampel@gmail.com>
2022-07-14 15:54:34 +0200

    LoginController.java: Changed the default request mapping to /api/v1

--- quishield/src/main/java/de/quichris/quishield/controller/LoginController.java
+++ quishield/src/main/java/de/quichris/quishield/controller/LoginController.java
@@ -1,15 +1,20 @@
 package de.quichris.quishield.controller;
 
 
+import de.quichris.quishield.entity.Account;
+import de.quichris.quishield.exceptions.AccountNotFound;
+import de.quichris.quishield.exceptions.WrongPassword;
 import de.quichris.quishield.service.AccountService;
+import de.quichris.quishield.util.loginRequestBody;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class LoginController {
 
