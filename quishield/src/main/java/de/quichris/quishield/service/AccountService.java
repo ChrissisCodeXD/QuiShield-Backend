ad3803fd3566ec0b2a7c39946da9c50808b02730
Chrissi <schwarzlichtampel@gmail.com>
2022-07-15 03:42:43 +0200

    AccountRepo.java: Added a lot of new functions

--- quishield/src/main/java/de/quichris/quishield/service/AccountService.java
+++ quishield/src/main/java/de/quichris/quishield/service/AccountService.java
@@ -1,14 +1,34 @@
 package de.quichris.quishield.service;
 
+import de.quichris.quishield.body.ChangeAccountPasswordRequestBody;
+import de.quichris.quishield.body.LoginRequestBody;
 import de.quichris.quishield.entity.Account;
-import de.quichris.quishield.exceptions.AccountDuplicate;
-import de.quichris.quishield.exceptions.AccountNotFound;
-import de.quichris.quishield.exceptions.WrongPassword;
+import de.quichris.quishield.entity.Password;
+import de.quichris.quishield.exceptions.*;
+
+import java.security.Key;
+import java.util.List;
 
 public interface AccountService {
 
     Account saveAccount(Account account) throws AccountDuplicate;
 
-    Account accountLogin(String email, String Password) throws AccountNotFound, WrongPassword;
+    Account accountLogin(LoginRequestBody loginRequestBody) throws AccountNotFound, WrongPassword;
 
+
+    Account getAccount(String token) throws AccountNotFound;
+
+    Account updateAccountPassword(ChangeAccountPasswordRequestBody account, String token) throws AccountNotFound, WrongPassword;
+
+    Key getKey();
+
+    List<Password> getPasswords(String token) throws AccountNotFound;
+
+    Password addPassword(Password password);
+
+    Password getPassword(String token, String password_id) throws PasswordNotFound, NotAuthorized;
+
+
+
+    Password updatePassword(Password password, String token) throws PasswordNotFound, NotAuthorized;
 }
