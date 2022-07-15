a43eba6d312f11e2a730df917075cecf94637149
Chrissi <schwarzlichtampel@gmail.com>
2022-07-15 03:46:05 +0200

    LoginController.java: added the /login route and /accounts/password route

--- quishield/src/main/java/de/quichris/quishield/controller/LoginController.java
+++ quishield/src/main/java/de/quichris/quishield/controller/LoginController.java
@@ -1,21 +1,27 @@
 package de.quichris.quishield.controller;
 
 
+import de.quichris.quishield.body.ChangeAccountPasswordRequestBody;
+import de.quichris.quishield.body.CreateAccountRequestBody;
 import de.quichris.quishield.entity.Account;
+import de.quichris.quishield.exceptions.AccountDuplicate;
 import de.quichris.quishield.exceptions.AccountNotFound;
 import de.quichris.quishield.exceptions.WrongPassword;
 import de.quichris.quishield.service.AccountService;
-import de.quichris.quishield.util.loginRequestBody;
+import de.quichris.quishield.body.LoginRequestBody;
+import de.quichris.quishield.body.LoginResponseBody;
+import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
+@Slf4j
 public class LoginController {
 
     private final AccountService accountService;
@@ -28,13 +34,33 @@ public class LoginController {
 
 
     @GetMapping("/login")
-    public ResponseEntity<Account> login(@RequestBody loginRequestBody lrb) throws AccountNotFound, WrongPassword {
-        String email = lrb.getEmail();
-        String password = lrb.getPassword();
-        
-        Account acc = accountService.accountLogin(email, password);
-        return ResponseEntity.ok(acc);
+    public ResponseEntity<LoginResponseBody> login(@RequestBody LoginRequestBody lrb) throws AccountNotFound, WrongPassword {
+        Account acc = accountService.accountLogin(lrb);
+        Date expireDate = new Date(System.currentTimeMillis() + 30 * 60000);
+        String token = Jwts.builder().setSubject(acc.getToken()).setExpiration(expireDate).signWith(accountService.getKey()).compact();
+        LoginResponseBody responseBody = new LoginResponseBody(token, acc.getUsername());
+        return ResponseEntity.ok(responseBody);
     }
 
+
+    @PutMapping("/accounts/password")
+    public ResponseEntity<Account> changePassword(@RequestBody ChangeAccountPasswordRequestBody body) throws AccountNotFound, WrongPassword {
+        String tok = Jwts.parserBuilder().setSigningKey(accountService.getKey()).build().parseClaimsJws(body.getToken()).getBody().getSubject();
+        Account tmp = accountService.updateAccountPassword(body, tok);
+        return ResponseEntity.ok(tmp);
+    }
+
+    @PostMapping("/accounts")
+    public ResponseEntity<Account> create_account(@RequestBody CreateAccountRequestBody carb) throws AccountDuplicate {
+        Account acc = new Account();
+        acc.setEmail(carb.getEmail());
+        acc.setPassword(carb.getPassword());
+        acc.setUsername(carb.getUsername());
+        if (carb.getProfilepicture() != null) {
+            acc.setProfilpicture(carb.getProfilepicture());
+        }
+        Account acc2 = accountService.saveAccount(acc);
+        return ResponseEntity.ok(acc2);
+    }
 
 }
