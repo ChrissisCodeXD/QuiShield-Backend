9fa7ec9d3962ce9d450d7a534ace6ed5afdcde60
Chrissi <schwarzlichtampel@gmail.com>
2022-07-15 03:48:10 +0200

    PasswordRepo.java: added getPasswordsByOwner and getPasswordById functions

--- quishield/src/main/java/de/quichris/quishield/repo/PasswordRepo.java
+++ quishield/src/main/java/de/quichris/quishield/repo/PasswordRepo.java
@@ -3,9 +3,13 @@ package de.quichris.quishield.repo;
 import de.quichris.quishield.entity.Password;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface PasswordRepo extends JpaRepository<Password,String> {
-    Optional<Password> findById(String id);
+public interface PasswordRepo extends JpaRepository<Password, String> {
+
+    List<Password> getPasswordsByOwner(String owner);
+
+    Optional<Password> getPasswordById(String id);
 
 }
