de5a8fe8b4641e40a64ec553a3930c6b8b7da717
Chrissi <schwarzlichtampel@gmail.com>
2022-07-15 03:48:53 +0200

    Reformat code

--- quishield/src/main/java/de/quichris/quishield/QuishieldApplication.java
+++ quishield/src/main/java/de/quichris/quishield/QuishieldApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class QuishieldApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QuishieldApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(QuishieldApplication.class, args);
+    }
 
 }
